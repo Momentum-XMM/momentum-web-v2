aa5b44217d2145af926432edc1f408f8c295faab
alex <alex@momentumdao.com>
2020-10-15 15:15:34 -0700

    About page style/content update

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
-// import styled from 'styled-components';
+import styled from 'styled-components';
 import UniLink from '../common/UniLink';
 import Infograpic from '../../pdfs/MomentumExplained.pdf';
 import AuditReport from '../../pdfs/MomentumAuditReport.pdf';
 import WhitePaper from '../../pdfs/MomentumWhitepaper.pdf';
 import Roadmap from '../../pdfs/roadmap.png'
+
+const Container = styled.div`
+    p {
+        font-size: 20px;
+    }
+
+    a.btn {
+        font-size: 16px;
+    }
+`;
 
 const LinkButton = ({ link, text }) => {
     return (
@@ -21,7 +31,7 @@ const LinkButton = ({ link, text }) => {
 
 const About = () => {
     return (
-        <div class="row">
+        <Container class="row">
             <div class="col-lg-12">
                 <div class="buy-btn fordsktop text-right">
                     <UniLink />
@@ -35,11 +45,11 @@ const About = () => {
                 <div class="about-box text-white">
                     <div class="about-icon-img"><img src="images/about-icon1.png" /></div>
                     <h5 class="text-uppercase text-white">MOMENTUM EXPLAINED</h5>
-                    <p class="t-sky">Momentum is a dynamically deflating token - the dynamic mechanism is called variable mometum burn</p>
+                    <p class="t-sky">Momentum is a dynamic deflationary token</p>
                     <p>Two important values are maintained in the token contract: the Short Momentum and the Long Momentum, and token transfers will have an impact on these values</p>
                     <p>Transfers that move the Short Momentum closer to the Long Momentum will have a transfer fee of ~0.75%</p>
-                    <p>Transfers that move the Short Momentum further away from the Long Momentum will be charged a destabilization fee between ~0.75% and ~3.5% on average and in rare cases a little bit more</p>
-                    <p>Both momentum values will be available on the home page or through functions on the contract</p>
+                    <p>Transfers that move the Short Momentum further away from the Long Momentum will be charged a destabilization fee between ~0.75% and ~3.5% on average, and in rare cases a little bit more</p>
+                    <p>Both momentum values will be available on the home page and through functions in the contract</p>
                     <LinkButton
                         link={Infograpic}
                         text="VIEW OUR VISUAL INFOGRAPHIC"
@@ -49,7 +59,7 @@ const About = () => {
                 <div class="about-box text-white">
                     <div class="about-icon-img"><img src="images/about-icon2.png" /></div>
                     <h5 class="text-uppercase text-white">MOMENTUM AUDITED</h5>
-                    <p>The contract has been audited by QuillHash and there were no critical/severe issues found. An audited protocol means investors have added security that contracts will not fail.</p>
+                    <p>The contract has been audited by QuillHash and there were no critical issues found</p>
                     <LinkButton
                         link={AuditReport}
                         text="VIEW AUDIT"
@@ -59,18 +69,24 @@ const About = () => {
                 <div class="about-box text-white">
                     <div class="about-icon-img"><img src="images/about-icon3.png" /></div>
                     <h5 class="text-uppercase text-white">MOMENTUM CONTRACTS</h5>
-                    <p>We beleive transparency is key - please find all contracts and proof of liquidity locked on Etherscan.</p>
+                    <p>We believe transparency is key - please find all contracts and proof of locked liquidity on Etherscan.</p>
                     <p>91% of initial total supply provided as liquidity and locked until December 21, 2021</p>
-                    <a href="https://etherscan.io/token/0xb469899812f74ee43bffe2d2022590111da86425#balances" target="_blank" class="btn">VIEW LIQUIDITY LOCKED</a>
-                    <a href="https://etherscan.io/address/0xfdba6feecdf75d2be2595a048141da210b324315#tokentxns" target="_blank" class="btn">LOCKING TRANSFER TX</a>
-                    <a href="https://etherscan.io/address/0xfdba6feecdf75d2be2595a048141da210b324315#readContract" target="_blank" class="btn">RELEASE TIME</a>
-                    
+                    <LinkButton
+                        link="https://etherscan.io/token/0xb469899812f74ee43bffe2d2022590111da86425#balances"
+                        text="VIEW LOCKED LIQUIDITY"
+                    />
+                    <LinkButton
+                        link="https://etherscan.io/address/0xfdba6feecdf75d2be2595a048141da210b324315#tokentxns"
+                        text="LOCKING TRANSFER TX"
+                    />
+                    <LinkButton
+                        link="https://etherscan.io/address/0xfdba6feecdf75d2be2595a048141da210b324315#readContract"
+                        text="RELEASE TIME"
+                    />
                 </div>
                 <div class="about-box text-white">
                     <div class="about-icon-img"><img src="images/about-icon4.png" /></div>
                     <h5 class="text-uppercase text-white">MOMENTUM WHITEPAPER & ROADMAP</h5>
-                    <p>We beleive transparency is key - please find all contracts and proof of liquidity locked on Etherscan.</p>
-                    <p>91% of initial total supply provided as liquidity and locked until December 21, 2021</p>
                     <LinkButton
                         link={WhitePaper}
                         text="MOMENTUM WHITEPAPER"
@@ -81,7 +97,7 @@ const About = () => {
                     />
                 </div>
             </div>
-        </div>
+        </Container>
     );
 }
 
