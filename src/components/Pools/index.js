d9017ca03c5214ad8f5aaa74365c850bf134caea
alex <alex@momentumdao.com>
2020-10-17 00:11:13 -0700

    Pool cards updated

--- src/components/Pools/index.js
+++ src/components/Pools/index.js
@@ -6,8 +6,13 @@ const StyledPoolCard = styled.div`
     font-family: 'Open Sans';
 
     .pooling-box {
-        padding: 40px 20px;
+        padding: 20px;
         border: 1px solid rgba(255,255,255,0.45);
+        display: flex;
+        flex-direction: column;
+        justify-content: space-evenly;
+        align-items: center;
+        text-align: center;
 
         .title {
             font-family: 'Open Sans';
@@ -42,7 +47,7 @@ const StyledPoolCard = styled.div`
 const PoolCard = ({ title, text, subtext, link1, link1text, link2, link2text }) => {
     return (
         <StyledPoolCard className="col-md-4 col-sm-6 text-white">
-            <div className="pooling-box text-center">
+            <div className="pooling-box">
                 <h5 className="text-white title">
                     {title}
                 </h5>
@@ -52,22 +57,24 @@ const PoolCard = ({ title, text, subtext, link1, link1text, link2, link2text }) 
                 {subtext && <p className="subtext">
                     {subtext}
                 </p>}
-                <a
-                    href={link1}
-                    className="btn"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    {link1text}
-                </a>
-                {link2 && <a
-                    href={link2}
-                    className="btn"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    {link2text}
-                </a>}
+                <div>
+                    <a
+                        href={link1}
+                        className="btn"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        {link1text}
+                    </a>
+                    {link2 && <a
+                        href={link2}
+                        className="btn"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        {link2text}
+                    </a>}
+                </div>
             </div>
         </StyledPoolCard>
     );
@@ -93,30 +100,30 @@ const Pools = () => {
                 <PoolCard
                     title="🔥 XMM / ETH 🔥"
                     text="Uniswap Pair"
-                    subtext="0x9a7a4c141a3bcce4a31e42c1192ac6add35069b4"
+                    // subtext="0x9a7a4c141a3bcce4a31e42c1192ac6add35069b4"
                     link1="https://app.uniswap.org/#/swap?inputCurrency=ETH&outputCurrency=0x9a7a4c141a3bcce4a31e42c1192ac6add35069b4"
                     link1text="Buy"
                     link2="https://app.uniswap.org/#/add/0x9a7a4C141a3BCCE4A31e42C1192Ac6Add35069b4/ETH"
-                    link2text="Add Liquidity"
+                    link2text="Add"
                 />
                 <PoolCard
-                    title="🌊 XMMx / ETH 🌊"
+                    title="XMMx / ETH"
                     text="XMMx = XMM/ETH Uniswap LP"
-                    subtext="0xb469899812f74ee43bffe2d2022590111da86425"
+                    // subtext="0xb469899812f74ee43bffe2d2022590111da86425"
                     link1="https://app.uniswap.org/#/swap?inputCurrency=ETH&outputCurrency=0xb469899812f74ee43bffe2d2022590111da86425"
                     link1text="Buy"
                     // link2="https://app.uniswap.org/#/add/0xb469899812f74ee43bffe2d2022590111da86425/ETH"
                     // link2text="Pool"
                 />
                 <PoolCard
-                    title="🎁 DeFiat Rewards Pool 🎁"
+                    title="DeFiat Rewards Pool"
                     text="Stake XMMx => Earn XMM"
                     subtext={<img src="images/defiat-logo.png" height="40px" />}
                     link1="https://defiat.net/dashboard/partners/0xfe78ebd1fe6ab976c058a795d9683d85c3929aed"
                     link1text="Rewards Pool"
                 />
                 <PoolCard
-                    title="⚖️ XMMx / ETH / LINK / SNX ⚖️"
+                    title="XMMx / ETH / LINK / SNX"
                     text="Balancer Pool"
                     subtext="25/25/25/25 split"
                     link1="https://pools.balancer.exchange/#/pool/0x953500397dbd43bf64321c4f54cd7b3f862c1bd6/"
@@ -125,7 +132,7 @@ const Pools = () => {
                     // link2text="Pools.FYI"
                 />
                 <PoolCard
-                    title="⚖️ XMMx / WBTC ⚖️"
+                    title="XMMx / WBTC"
                     text="Balancer Pool"
                     subtext="34/66 split"
                     link1="https://pools.balancer.exchange/#/pool/0xc3d407ce0a06530315c14c9f2374a16cf436461a/"
